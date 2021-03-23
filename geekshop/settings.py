02db5a989e1a225d515eaeda4ef320f80d79d8e9
Ilusion <by_Ilusion@mail.ru>
2021-03-23 19:54:51 +0300

    Temporary using caching full site

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -58,6 +58,7 @@ if DEBUG:
 AUTH_USER_MODEL = 'userapp.User'
 
 MIDDLEWARE = [
+    # "django.middleware.cache.UpdateCacheMiddleware",  # for entire site caching
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "social_django.middleware.SocialAuthExceptionMiddleware",
+    # "django.middleware.cache.FetchFromCacheMiddleware",  # for entire site caching
 ]
 
 if DEBUG:
